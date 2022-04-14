4efe9b72a16b506498296ac658d30c35daf4909d
henson-alex <henson.alex@ibm.com>
2022-04-14 17:40:05 -0500

    working on d3 to create charts

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,81 @@ const DUMMY_DATA = [
   { id: 'd2', value: 11, region: 'India' },
   { id: 'd3', value: 12, region: 'China' },
   { id: 'd4', value: 6, region: 'Germany' },
-]
+];
 
-d3.select('div')
-  .selectAll('p')
+const xScale = d3
+  .scaleBand()
+  .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+  .rangeRound([0, 250])
+  .padding(0.1);
+
+const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
+
+const container = d3.select('svg').classed('container', true);
+
+const bars = container
+  .selectAll('.bar')
   .data(DUMMY_DATA)
-  .enter()
-  .append('p')
-  .text(dta => dta.region);
+  .enter() //gives you missing elements that were not rendered yet
+  .append('rect')
+  .classed('bar', true)
+  .attr('width', xScale.bandwidth())
+  .attr('height', (data) => 200 - yScale(data.value))
+  .attr('x', (data) => xScale(data.region))
+  .attr('y', (data) => yScale(data.value));
 
-// alert('this works in public js!');
+// setTimeout(() => {
+//   bars.data(DUMMY_DATA.slice(0, 2)).exit().remove();
+// }, 2000);
+
+
+
+  const countryData = {
+    items: ['China', 'India', 'USA'],
+    addItem(item) {
+      this.items.push(item); 
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+    updateItem(index, newItem) {
+      this.items[index] = newItem;
+    }  
+  }
+
+  d3.select('#d3')
+    .selectAll('li')
+    .data(countryData.items, data => data)
+    .enter()
+    .append('li')
+    .text(data => data);
+
+  setTimeout(() => {
+    countryData.addItem('Germany');
+    d3.select('#d3')
+      .selectAll('li')
+      .data(countryData.items, data => data)
+      .enter()
+      .append('li')
+      .classed('added', true)
+      .text(data => data);
+  }, 2000);
+
+  setTimeout(() => {
+    countryData.removeItem(0);
+    d3.select('#d3')
+      .selectAll('li')
+      .data(countryData.items, data => data)
+      .exit()
+      .classed('redundant', true)
+  }, 4000);
+
+  setTimeout(() => {
+    countryData.updateItem(1, 'Russia');
+    d3.select('#d3')
+      .selectAll('li')
+      .data(countryData.items, data => data)
+      .exit()
+      .classed('updated', true)
+      .text('Russia');
+  }, 6000);
