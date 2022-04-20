9a0e3a5a2534ee9e9662eff103caacbde0453979
henson-alex <henson.alex@ibm.com>
2022-04-19 20:29:45 -0500

    completed bar and pie chart

--- public/js/index.js
+++ public/js/index.js
@@ -1,85 +1,4 @@
-// const DUMMY_DATA = [
-//   { id: 'd1', value: 10, region: 'USA' },
-//   { id: 'd2', value: 11, region: 'India' },
-//   { id: 'd3', value: 12, region: 'China' },
-//   { id: 'd4', value: 6, region: 'Germany' },
-// ];
-
-// const xScale = d3
-//   .scaleBand()
-//   .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
-//   .rangeRound([0, 250])
-//   .padding(0.1);
-
-// const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
-
-// const container = d3.select('svg').classed('container', true);
-
-// const bars = container
-//   .selectAll('.bar')
-//   .data(DUMMY_DATA)
-//   .enter() //gives you missing elements that were not rendered yet
-//   .append('rect')
-//   .classed('bar', true)
-//   .attr('width', xScale.bandwidth())
-//   .attr('height', (data) => 200 - yScale(data.value))
-//   .attr('x', (data) => xScale(data.region))
-//   .attr('y', (data) => yScale(data.value));
-
-// // setTimeout(() => {
-// //   bars.data(DUMMY_DATA.slice(0, 2)).exit().remove();
-// // }, 2000);
-
-//   const countryData = {
-//     items: ['China', 'India', 'USA'],
-//     addItem(item) {
-//       this.items.push(item);
-//     },
-//     removeItem(index) {
-//       this.items.splice(index, 1);
-//     },
-//     updateItem(index, newItem) {
-//       this.items[index] = newItem;
-//     }
-//   }
-
-//   d3.select('#d3')
-//     .selectAll('li')
-//     .data(countryData.items, data => data)
-//     .enter()
-//     .append('li')
-//     .text(data => data);
-
-//   setTimeout(() => {
-//     countryData.addItem('Germany');
-//     d3.select('#d3')
-//       .selectAll('li')
-//       .data(countryData.items, data => data)
-//       .enter()
-//       .append('li')
-//       .classed('added', true)
-//       .text(data => data);
-//   }, 2000);
-
-//   setTimeout(() => {
-//     countryData.removeItem(0);
-//     d3.select('#d3')
-//       .selectAll('li')
-//       .data(countryData.items, data => data)
-//       .exit()
-//       .classed('redundant', true)
-//   }, 4000);
-
-//   setTimeout(() => {
-//     countryData.updateItem(1, 'Russia');
-//     d3.select('#d3')
-//       .selectAll('li')
-//       .data(countryData.items, data => data)
-//       .exit()
-//       .classed('updated', true)
-//       .text('Russia');
-//   }, 6000);
-
+// BAR CHART
 d3.csv('data/cars.csv').then((data) => {
   const carMap = new Map();
 
@@ -96,20 +15,21 @@ d3.csv('data/cars.csv').then((data) => {
 
   carArrOfObj.splice(10);
 
-  console.log(carArrOfObj);
-
-  // const DUMMY_DATA = [
-  //   { id: 'd1', value: 10, region: 'USA' },
-  //   { id: 'd2', value: 11, region: 'India' },
-  //   { id: 'd3', value: 12, region: 'China' },
-  //   { id: 'd4', value: 6, region: 'Germany' },
-  // ];
+  const sortedArr = carArrOfObj.sort((x, y) => {
+    if (x.make < y.make) {
+      return -1;
+    }
+    if (x.make > y.make) {
+      return 1;
+    }
+    return 0;
+  });
 
   const MARGINS = { top: 20, bottom: 10 };
   const CHART_WIDTH = 800;
-  const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
+  const CHART_HEIGHT = 500 - MARGINS.top - MARGINS.bottom;
 
-  let selectedData = carArrOfObj;
+  let selectedData = sortedArr;
 
   const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1); //makes equally sized items along x axis
   const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
@@ -119,8 +39,8 @@ d3.csv('data/cars.csv').then((data) => {
     .attr('width', CHART_WIDTH)
     .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
 
-  x.domain(carArrOfObj.map((d) => d.make));
-  y.domain([0, d3.max(carArrOfObj, (d) => d.count) + 3]);
+  x.domain(sortedArr.map((d) => d.make));
+  y.domain([0, d3.max(sortedArr, (d) => d.count) + 3]);
 
   const chart = chartContainer.append('g');
 
@@ -128,7 +48,7 @@ d3.csv('data/cars.csv').then((data) => {
     .append('g')
     .call(d3.axisBottom(x).tickSizeOuter(0))
     .attr('transform', `translate(0, ${CHART_HEIGHT})`)
-    .attr('color', '#4f009e');
+    .attr('color', 'black');
 
   function renderChart() {
     chart
@@ -150,122 +70,64 @@ d3.csv('data/cars.csv').then((data) => {
 
     chart
       .selectAll('.label')
-      .data(selectedData, (data) => data.make)
+      .data(selectedData, (data) => data.count)
       .enter()
       .append('text')
-      .text((data) => data.value)
+      .text((data) => data.count)
       .attr('x', (data) => x(data.make) + x.bandwidth() / 2)
-      .attr('y', (data) => y(data.count) - 20)
+      .attr('y', (data) => y(data.count) - 10)
       .attr('text-anchor', 'middle')
       .classed('label', true);
 
     chart
       .selectAll('.label')
-      .data(selectedData, (data) => data.make)
+      .data(selectedData, (data) => data.count)
       .exit()
       .remove();
   }
-
   renderChart();
 });
-// let unselectedIds = [];
-
-// Creates the containers for the checkboxes
-// const listItems = d3
-//   .select('#data')
-//   .select('ul')
-//   .selectAll('li')
-//   .data(DUMMY_DATA)
-//   .enter()
-//   .append('li');
-
-// Labels next to the checkboxes
-// listItems.append('span').text((data) => data.region);
-
-// Creates the check boxes
-// listItems
-//   .append('input')
-//   .attr('type', 'checkbox')
-//   .attr('checked', true)
-//   .on('change', (data) => {
-//     if (unselectedIds.indexOf(data.id) === -1) {
-//       unselectedIds.push(data.id);
-//     } else {
-//       unselectedIds = unselectedIds.filter((id) => id !== data.id);
-//     }
-//     selectedData = DUMMY_DATA.filter((d) => unselectedIds.indexOf(d.id) === -1);
-//     renderChart();
-//   });
 
 // Pie Chart
-// d3 code goes here
-// const svg = d3.select('svg'),
-//   width = svg.attr('width'),
-//   height = svg.attr('height'),
-//   radius = Math.min(width, height) / 2;
+const svg = d3.select('.pie'),
+  width = svg.attr('width'),
+  height = svg.attr('height'),
+  radius = Math.min(width, height) / 2;
 
-// const g = svg
-//   .append('g')
-//   .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+const g = svg
+  .append('g')
+  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-// const color = d3.scaleOrdinal(['#C7CEEA', '#B5EAD7', '#FFDAC1', '#FF9AA2']);
-// const pie = d3.pie().value((d) => {
-//   d.valueOf()[1];
-// });
+const color = d3.scaleOrdinal(['#C7CEEA', '#B5EAD7', '#FFDAC1', '#FF9AA2']);
+const pie = d3.pie().value((d) => d.percent);
+const path = d3
+  .arc()
+  .outerRadius(radius - 40)
+  .innerRadius(0);
+const label = d3
+  .arc()
+  .outerRadius(radius)
+  .innerRadius(radius - 150);
+d3.csv('/data/cars_percent.csv').then((data) => {
+  var arc = g
+    .selectAll('.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc');
+  arc
+    .append('path')
+    .attr('d', path)
+    .attr('fill', (d) => color(d.data.status));
 
-// const carList = [];
-
-// const path = d3
-//   .arc()
-//   .outerRadius(radius - 40)
-//   .innerRadius(0);
-// const label = d3
-//   .arc()
-//   .outerRadius(radius)
-//   .innerRadius(radius - 150);
-
-// d3.csv('data/cars.csv').then((data) => {
-//   const carMap = new Map();
-
-//   for (let obj of data) {
-//     carname = obj.carname.split(' ')[0];
-//     carMap.set(carname, carMap.has(carname) ? carMap.get(carname) + 1 : 1);
-//   }
-
-//   for (let carname of carMap) {
-//     carMap.set(carname[0], (carMap.get(carname[0]) / data.length).toFixed(3));
-//   }
-
-//   const arc = g
-//     .selectAll('.arc')
-//     .data(pie(carMap))
-//     .enter()
-//     .append('g')
-//     .attr('class', 'arc');
-
-//   console.log(carMap.keys());
-//   arc
-//     .append('path')
-//     .attr('d', path)
-//     .attr('fill', (d) => color(d.data.company));
-
-//   arc
-//     .append('text')
-//     .attr('transform', (d) => 'translate(' + label.centroid(d) + ')')
-//     .text(carMap.keys());
-
-//   svg
-//     .append('g')
-//     .attr('transform', 'translate(' + (width / 2 - 120) + ',' + 20 + ')')
-//     .append('text')
-//     .text('Car Makes of Dataset')
-//     .attr('class', 'title');
-// });
-
-// console.log(carList);
-// console.log('length', carList.length);
-// console.log(Array.isArray(carList));
-// console.log(carList[0]);
-// let carSet = new Set();
-// carList.map((item) => carSet.add(item));
-// console.log(carSet);
+  arc
+    .append('text')
+    .attr('transform', (d) => 'translate(' + label.centroid(d) + ')')
+    .text((d) => d.data.status);
+  svg
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2 - 180) + ',' + 20 + ')')
+    .append('text')
+    .text('Emmissions Pass/Fail Percentage')
+    .attr('class', 'title');
+});
