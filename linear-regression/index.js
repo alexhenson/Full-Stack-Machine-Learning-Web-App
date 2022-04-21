53f30ddcffd90dcef15a3afec4f09d23c5811e52
henson-alex <henson.alex@ibm.com>
2022-04-20 23:24:46 -0500

    need to export linear-regression functions

--- linear-regression/index.js
+++ linear-regression/index.js
@@ -4,30 +4,48 @@ const loadCSV = require('../util/load-csv');
 const LinearRegression = require('./linear-regression');
 const plot = require('node-remote-plot');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('../public/data/cars.csv', {
-  shuffle: true,
-  splitTest: 100,
-  dataColumns: ['horsepower', 'weight', 'displacement'],
-  labelColumns: ['mpg'],
-});
+let regression = null;
 
-const regression = new LinearRegression(features, labels, {
-  learningRate: 0.1,
-  iterations: 30,
-  batchSize: 10,
-});
+const trainAndTest = () => {
+  let { features, labels, testFeatures, testLabels } = loadCSV(
+    '../public/data/cars.csv',
+    {
+      shuffle: true,
+      splitTest: 100,
+      dataColumns: ['horsepower', 'weight', 'displacement'],
+      labelColumns: ['mpg'],
+    }
+  );
 
-regression.train();
+  regression = new LinearRegression(features, labels, {
+    learningRate: 0.1,
+    iterations: 30,
+    batchSize: 10,
+  });
 
-const r2 = regression.test(testFeatures, testLabels);
+  regression.train();
 
-plot({
-  x: regression.mseHistory.reverse(),
-  xLabel: 'Iteration #',
-  yLabel: 'Mean Squared Error',
-});
+  const r2 = regression.test(testFeatures, testLabels);
 
-console.log('R2 is', r2);
+  plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error',
+  });
 
-regression.predict([[120, 2, 380]]).print();
+  return (r2 * 100).toFixed(2) + '%';
+};
 
+const makePrediction = (horsepower, weight, displacement) =>
+  regression
+    .predict([[horsepower, weight, displacement]])
+    .dataSync()[0]
+    .toFixed(2);
+
+// console.log(trainAndTest());
+// console.log(makePrediction(100, 2, 300));
+
+module.exports = {
+  trainAndTest: function () {},
+  predict: function () {},
+};
---
+++ public/js/predictive.js
@@ -0,0 +1,12 @@
+// const { trainAndTest, predict } = require('../../linear-regression/index');
+
+// const linearRegression = require('../../linear-regression/index');
+// const trainAndTest = linearRegression.trainAndTest;
+
+const horsepower = document.getElementById('horsepower').value;
+const weight = document.getElementById('weight').value;
+const displacement = document.getElementById('displacement').value;
+
+// console.log(trainAndTest());
+
+// need to figure out how to export functions
